03d047985894924ba9e6edf14cab8f2d9d958a3f
ben <benfherm@gmail.com>
2020-10-05 11:16:59 -0700

    ..

--- __tests__/ContactDetails.test.js
+++ __tests__/ContactDetails.test.js
@@ -22,20 +22,3 @@ it('renders correctly', () => {
   });
   
   });
-
-
-
-// test('form submits two answers', () => {
-//     const mockFn = jest.fn();
-  
-//     const answerInputs = getAllByA11yLabel('answer input');
-  
-//     fireEvent.changeText(answerInputs[0], 'a1');
-//     fireEvent.changeText(answerInputs[1], 'a2');
-//     fireEvent.press(getByText('Submit'));
-  
-//     expect(mockFn).toBeCalledWith({
-//       '1': { q: 'q1', a: 'a1' },
-//       '2': { q: 'q2', a: 'a2' },
-//     });
-//   });
